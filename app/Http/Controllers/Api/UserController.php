e66ae00180d165dd97efb7bba26147fed70c982d
Rossana Liendo <rossana.liendo@gmail.com>
2024-08-07 12:02:31 +0200

    Starting with my GitFlow

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class UserController extends Controller
 {
+    use HasApiTokens, HasFactory, Notifiable;
+
     //POST [username, email, password]
     public function register(Request $request)
     {
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
+//Open Routes
+Route::post("register", [UserController::class, "register"]);
+Route::post("register", [UserController::class, "login"]);
+
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:api');
+})->middleware('auth:api'); */
