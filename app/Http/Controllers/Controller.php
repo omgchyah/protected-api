80250d5fc0e674920623c0a9617c6d25b6c0a42e
Rossana Liendo <rossana.liendo@gmail.com>
2024-08-07 11:33:18 +0200

    making method for my api controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    //POST [username, email, password]
+    public function register(Request $request)
+    {
+
+    }
+
+        //POST [email, password]
+        public function login(Request $request)
+        {
+    
+        }
+
+            //POST [name, email, password]
+        public function register(Request $request)
+        {
+
+        }
+
+        
 }
